ce7eb18bfff7afd8cea496d22c50b798451a0e5c
ElsoOnihart <joaopedrotitan970@gmail.com>
2025-10-28 13:26:06 -0300

    Server update commit
    update Server

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,23 @@ import 'dotenv/config';
 import cors from 'cors';
 import { connectDB } from './database/connection';
 import masterRouter from './routes';
+import authRoutes from './routes/authRoutes'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`API rodando na porta ${PORT}`);
+});
 
 connectDB(); 
 
 app.use(cors());
 app.use(express.json());
 app.use('/', masterRouter);
+app.use(express.json());
+app.use('/v1/auth', authRoutes);
+
+
 
 app.listen(PORT, () => {
     console.log(`🚀 API rodando no http://localhost:${PORT}`);
