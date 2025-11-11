bcd21058f0b4032981f015f7dd8756cec154e5b6
ElsoOnihart <joaopedrotitan970@gmail.com>
2025-11-11 02:26:41 -0300

    panicRepository.ts commit
    19 -busca no banco de dados o email e o nome de todos os contatos de emergência de um paciente específico.

--- src/database/repositories/panicRepository.ts
+++ src/database/repositories/panicRepository.ts
@@ -4,9 +4,9 @@ import { IPanicEvent } from '../../models/PanicEventModel';
 
 export const createPanicLog = async (panicData: Omit<IPanicEvent, 'id_panico' | 'timestamp'>): Promise<number | null> => {
     const { usuario_id, latitude, longitude } = panicData;
-    
+
     const sql = 'INSERT INTO EVENTO_PANICO (usuario_id, latitude, longitude) VALUES (?, ?, ?)';
-    
+
     try {
         const [result]: any = await pool.query(sql, [usuario_id, latitude, longitude]);
         return result.insertId;
@@ -16,24 +16,26 @@ export const createPanicLog = async (panicData: Omit<IPanicEvent, 'id_panico' | 
     }
 };
 
-
-export const getEmergencyContacts = async (userId: number): Promise<{ whatsapp_numero: string }[]> => {
-    
-    const sql = 'SELECT whatsapp_numero FROM CONTATO_EMERGENCIA WHERE id_paciente = ?';
-    
+export const getEmergencyContactsData = async (userId: number): Promise<{ email: string, nome: string }[]> => {
+    const sql = `
+        SELECT u.email, u.nome 
+        FROM CONTATO_EMERGENCIA ce
+        JOIN USUARIO u ON ce.id_contato = u.id_usuario
+        WHERE ce.id_paciente = ?;
+    `;
     try {
         const [rows]: any = await pool.query(sql, [userId]);
         
         return rows; 
     } catch (error) {
-        console.error("Erro ao buscar contatos de emergência:", error);
+        console.error("Erro ao buscar dados dos contatos de emergência:", error);
         throw new Error('Erro ao buscar contatos de emergência.');
     }
 };
 
 
 export const getPanicLogsByUserId = async (userId: number): Promise<IPanicEvent[]> => {
-    
+
     const sql = 'SELECT * FROM EVENTO_PANICO WHERE usuario_id = ? ORDER BY timestamp DESC';
     
     try {
