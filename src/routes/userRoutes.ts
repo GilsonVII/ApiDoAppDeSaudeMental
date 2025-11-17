e762e799d595a7fac95a3d24c4036a2b41d9e62a
ElsoOnihart <joaopedrotitan970@gmail.com>
2025-11-17 18:31:20 -0300

    Registra endpoints de delete contato e patch token
    Linhas (13-14): Adição de duas novas linhas de rotas.
    
    DELETE /contact/:id_relacao: Vincula a rota ao handleDeleteContact.
    
    PATCH /fcm-token: Vincula a rota ao handleUpdateFcmToken

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
-import { handleGetMyProfile, handleAddContact, handleListContacts, handleUpdateFcmToken,
-     handleUpdateProfile, handleSearchUser } from '../controllers/userController';
+import { handleGetMyProfile, handleAddContact, handleListContacts, handleUpdateFcmToken, handleUpdateProfile, handleSearchUser, handleDeleteContact } from '../controllers/userController';
 import { authMiddleware } from '../middlewares/authMiddleware';
 
 const userRouter = Router();
 userRouter.patch('/me', authMiddleware, () => {});
-
 userRouter.get('/me', authMiddleware, handleGetMyProfile);
 userRouter.post('/contact', authMiddleware, handleAddContact);
 userRouter.get('/contacts', authMiddleware, handleListContacts);
 userRouter.patch('/fcm-token', authMiddleware, handleUpdateFcmToken);
 userRouter.patch('/me', authMiddleware, handleUpdateProfile);
 userRouter.get('/search', authMiddleware, handleSearchUser);
+userRouter.delete('/contact/:id_relacao', authMiddleware, handleDeleteContact);
+userRouter.patch('/fcm-token', authMiddleware, handleUpdateFcmToken);
 
 export default userRouter;
