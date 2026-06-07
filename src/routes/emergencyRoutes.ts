d2f2ee3a9e8737e0f01ab6f5a6226d6b4551cc1c
Gilson Alberice <gilsonjalberice@gmail.com>
2026-06-06 22:28:17 -0300

    adc rota handleGetPanicLogs

--- src/routes/emergencyRoutes.ts
+++ src/routes/emergencyRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { handleTriggerPanic } from '../controllers/emergencyController';
 import { authMiddleware } from '../middlewares/authMiddleware';
+import { handleGetPanicLogs } from '../controllers/emergencyController';
 
 const emergencyRouter = Router();
 
 emergencyRouter.post('/panic/trigger', authMiddleware, handleTriggerPanic);
+emergencyRouter.get('/panic/logs/:id_paciente', authMiddleware, handleGetPanicLogs);
 
 export default emergencyRouter;
